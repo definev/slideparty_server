c662e5cff9b617052fa7e809fe8a7a423b7aca7e
definev <daiduong.workmail@gmail.com>
2022-01-24 01:39:25 +0700

    chore: ready to deploy

--- web/server.dart
+++ web/server.dart
@@ -3,13 +3,6 @@ import 'package:shelf_router/shelf_router.dart';
 import 'package:slideparty_server/slideparty_socket_handler.dart';
 
 void main(List<String> args) async {
-  // final port = args[0];
-  // final mongoUrl = args[1];
-
-  // mongo = await Db.create(mongoUrl);
-  // await mongo.open();
-  print('MongoDB connected');
-
   final router = Router() //
     ..get(
         '/ws/<size>/<roomCode>',
@@ -17,7 +10,7 @@ void main(List<String> args) async {
             slidepartySocketHandler(size, roomCode)(request));
 
   sio
-      .serve(router, 'localhost', int.parse('8080')) //
+      .serve(router, '0.0.0.0', int.parse('8080')) //
       .then((server) =>
           print('Server is serving at ${server.address.host}:${server.port}'));
 }
