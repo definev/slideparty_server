9198c26b92123774f852794e6ab6456f15c1f857
definev <daiduong.workmail@gmail.com>
2022-01-24 13:53:04 +0700

    fix: heroku wrong port

--- bin/server.dart
+++ bin/server.dart
@@ -5,6 +5,9 @@ import 'package:shelf/shelf_io.dart' as sio;
 import 'package:shelf_router/shelf_router.dart';
 import 'package:slideparty_server/slideparty_socket_handler.dart';
 
+var portEnv = Platform.environment['PORT'];
+var _hostname = portEnv == null ? 'localhost' : '0.0.0.0';
+
 void main(List<String> args) async {
   final router = Router() //
     ..get('/', (_) => Response.ok('Welcome to slideparty!'))
@@ -14,7 +17,7 @@ void main(List<String> args) async {
             slidepartySocketHandler(size, roomCode)(request));
 
   sio
-      .serve(router, '0.0.0.0', int.parse('8080')) //
+      .serve(router, _hostname, int.parse(portEnv ?? '9999')) //
       .then((server) =>
           print('Server is serving at ${server.address.host}:${server.port}'));
 }
